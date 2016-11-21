53cd7e490f8b70a488cc20efe6b5611a0ed2410f
Jaehyung Ahn <jaehyung2016@users.noreply.github.com>
2016-11-21 13:01:27 -0600

    post show view updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,17 @@ class PostsController < ApplicationController
   before_action :require_login, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :check_permission, only: [:update, :destroy]
+  before_action :set_posts, only: [:index, :show]
+
+  helper_method :author_of?
+
+  ARTICLE_COUNT_PER_PAGE = 1
+  LIST_COUNT_PER_PAGE = 1
 
   # GET /posts
   # GET /posts.json
   def index
-    @articles = Post.order(:id).page(params[:page]).per(1)
-    @posts = Post.select(:id, :title).order(:id).page(params[:list_page]).per(1)
+    @articles = Post.order(:id).page(params[:page]).per(ARTICLE_COUNT_PER_PAGE)
   end
 
   # GET /posts/1
@@ -70,16 +75,24 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
 
+    def set_posts
+      @posts = Post.select(:id, :title).order(:id).page(params[:list_page]).per(LIST_COUNT_PER_PAGE)
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
       params.require(:post).permit(:title, :content, :user_id)
     end
 
     def check_permission
-      unless current_user == @post.user
+      unless author_of? @post
         unless params[:action] == "destroy" && admin?
           redirect_to root_path, alert: "Permission denied"
         end
       end
+    end
+
+    def author_of? article
+      current_user == article.user
     end
 end
